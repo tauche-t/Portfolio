3f56d0ad2a1c87a2a04e63ac68acdfc0bf016165
tauche-t <tauche.t3473@gmail.com>
2022-02-20 21:15:53 +0900

    scroll 및 기획서

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ const skillsTab = document.querySelectorAll('.skills_tab li');
 const skillsContents = document.getElementsByClassName('skills_content');
 let bar = document.getElementsByClassName('bar');
 
+// Scroll
+const header = document.getElementById("header");
+const headerHeight = header.offsetHeight;
+
+window.addEventListener('scroll', function() {
+  let windowTop = window.scrollY;
+  if(windowTop >= headerHeight) {
+    header.classList.add("scrollActive");
+  }else{
+    header.classList.remove("scrollActive");
+  }
+});
+
 Array.from(skillsTab).forEach((tab) => {
   tab.addEventListener('click', (e) => {
     const tabNum = tab.getAttribute('data-num');
@@ -25,6 +38,26 @@ function onTabBar(item) {
   bar[0].style.top = item.currentTarget.offsetTop +  item.currentTarget.offsetHeight + "px";
 }
 
+
+
+// Project PopUp & Hover
+const projectContents = document.querySelector('.project_contents');
+const projectOverview = document.querySelector('.project_overview');
+const overlay = document.querySelector('.overlay');
+const popupClose = document.querySelector('.popup_close');
+
+projectContents.addEventListener('mouseenter', function() {
+  projectOverview.classList.add("hover");
+});
+projectContents.addEventListener('mouseleave', function() {
+  projectOverview.classList.remove("hover");
+});
+projectContents.addEventListener('click', function() {
+  overlay.classList.remove('hide');
+});
+popupClose.addEventListener('click', function() {
+  overlay.classList.add('hide');
+})
 
 // FAQ accordion
 const faqContent = document.getElementsByClassName('faq_content');
